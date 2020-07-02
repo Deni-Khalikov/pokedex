d68c755d1d350fa53d8e4ade24bcbed42c076bc7
Nathan Van Gulck <vangulcknathan@gmail.com>
2020-07-02 14:55:26 +0200

    merging Deni's html into local

--- pokedex.js
+++ pokedex.js
@@ -2,7 +2,7 @@
 
 const getPokemon = () => {
     let pokeName = document.getElementById("input").value;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeName}`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -10,20 +10,20 @@ const getPokemon = () => {
 }
 
 const showRandomPokemon = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
+    fetch("https://pokeapi.co/api/v2/pokemon-species/")
         .then(response => response.json())
         .then(data => {
             let id = data.count;
             return id;
         })
         .then(id => {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * id)}`)
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${Math.floor(Math.random() * id)}`)
                 .then(response => response.json())
                 .then(data => {
                     alert(data.name)
                 })
         })
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
@@ -38,13 +38,20 @@ const showNext = () => {
     console.log("next")
 }
 
+const evolutions = () => {
+    fetch(`https://pokeapi.co/api/v2/evolution-chain/`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
 
 
 document.getElementById("search").addEventListener("click", getPokemon);
 document.getElementById("random").addEventListener("click", showRandomPokemon);
 document.getElementById("previous").addEventListener("click", showPrevious);
 document.getElementById("next").addEventListener("click", showNext)
-
+document.getElementById("evolution").addEventListener("click", evolutions)
 
 
 
